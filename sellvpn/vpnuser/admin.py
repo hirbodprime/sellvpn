0eb8dc6dadc8a300e58c58d7fb653503aaa4b726
hirbodprime <hirbodprime@gmail.com>
2025-06-26 15:52:49 +0330

    asfd

--- sellvpn/vpnuser/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import VPNConfig, VPNUser, SubscriptionPlan, PaymentSettings,VPNDelivery
-
-
-admin.site.register(VPNDelivery)
-admin.site.register(VPNUser)
-admin.site.register(VPNConfig)
-admin.site.register(SubscriptionPlan)
-admin.site.register(PaymentSettings)
