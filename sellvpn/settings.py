eede0f3f1607f0f5ac934a62fc849b25ff3ef8ed
hirbodprime <hirbodprime@gmail.com>
2025-06-26 15:57:25 +0330

    aasfd

--- sellvpn/settings.py
+++ sellvpn/settings.py
@@ -114,12 +114,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+import os
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
